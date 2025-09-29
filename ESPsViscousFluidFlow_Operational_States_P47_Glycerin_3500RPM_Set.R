a78fdc2224669406e564ba4b367868f14bed618e
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2025-09-29 15:03:19 -0300

    Update ESPsViscousFluidFlow_Operational_States_P47_Glycerin_3500RPM_Set.R

--- ESPsViscousFluidFlow_Operational_States_P47_Glycerin_3500RPM_Set.R
+++ ESPsViscousFluidFlow_Operational_States_P47_Glycerin_3500RPM_Set.R
@@ -15,6 +15,49 @@ merge_water_viscous_sub<-cbind(merge_water_viscous_sub,merge_water_viscous_sub_t
 merge_water_viscous_sub$operational_states<-paste(paste("n=",merge_water_viscous_sub$n_discrete,sep=""),paste("BHP=",merge_water_viscous_sub$BHP_discrete,sep=""),paste("H=",merge_water_viscous_sub$H_discrete,sep=""),sep="|")
 ##################################################################################################################################################################
 # Remove units from bhp, head and efficiency and put them in a single plot
+merge_water_viscous_normalized<-as.data.frame(lapply(merge_water_viscous_sub[,c("n","BHP","H")], normalize))
+
+# Renames collumns
+colnames(merge_water_viscous_normalized)<-c("n_normalized","BHP_normalized","H_normalized")
+
+# Merge tables
+merge_water_viscous_sub<-cbind(merge_water_viscous_sub,merge_water_viscous_normalized)
+
+# Add stepp
+merge_water_viscous_sub$step<-1:dim(merge_water_viscous_sub)[1]
+
+# Take collumns for the plot
+merge_water_viscous_plot<-merge_water_viscous_sub[,c("n_normalized","BHP_normalized","H_normalized","step")]
+
+# Rename collumns
+colnames(merge_water_viscous_plot)<-c("n","BHP","H","step")
+
+# Melt table
+merge_water_viscous_sub_melt<-reshape2::melt(merge_water_viscous_plot,id.vars="step")
+
+# Generate plot
+p1<-ggplot(merge_water_viscous_sub_melt, aes(x=step, y=value, group=variable)) +   geom_line(aes(color=variable))+   geom_point(aes(color=variable)) +  theme_bw() + theme(legend.position = "bottom") + ggtitle("P47, 3550RPM, Glycerin, inlet viscosituy 128")
+##################################################################################################################################################################
+# Take the discrete values
+merge_water_viscous_sub_tertiles$step<-1:dim(merge_water_viscous_sub_tertiles)[1]
+
+# Rename collumns
+colnames(merge_water_viscous_sub_tertiles)<-c("n","BHP","H","step")
+
+# Melt table
+merge_water_viscous_sub_tertiles<-reshape2::melt(merge_water_viscous_sub_tertiles,id.vars="step")
+
+@ Generate second
+p2<-ggplot(merge_water_viscous_sub_tertiles, aes(x = step, y = variable, fill = value)) +   geom_tile() +    theme_bw() + theme(legend.position = "bottom")
+
+
+
+# bwplot               
+png(filename=paste(output_dir,"Normalized_Discrete_Performance.png",sep=""), width = 15, height = 30, res=600, units = "cm")  
+  # Plot the bayesian network graph
+  ggarrange(p1, p2, labels = c("A", "B"),    ncol = 1, nrow = 2)
+dev.off()
+
 
 
 
