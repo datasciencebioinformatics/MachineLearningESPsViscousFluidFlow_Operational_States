bd0ba209b2184243e5872894810c4a52e95c691f
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2025-09-29 11:41:59 -0300

    Create MachineLearningESPsViscousFluidFlow_Operational_States_Load_All_R_Packages.R

---
+++ MachineLearningESPsViscousFluidFlow_Operational_States_Load_All_R_Packages.R
@@ -0,0 +1,63 @@
+library(readxl)
+library(ggplot2)
+library(reshape2)
+library(dplyr) 
+library("PerformanceAnalytics")
+library("MALDIquant")
+library("gridExtra")
+library("caret")
+library("hydraulics")
+library("CHNOSZ")
+library("gglm")
+library("rpart")
+library(stringr)
+library("bnlearn")
+library("rpart.plot")
+library("liver")
+library("e1071")
+library("ggplot2")         # Graphics engine
+library("RColorBrewer")    # Nice color palettes
+library("plot3D")          # for 3d surfaces. 
+library("dplyr")           # Better data manipulations
+library("tidyr")           # gather variables into long format
+library("parallel")        # mclapply for multicore processing
+library("randomForestSRC") # random forests for survival, regression and 
+library("OneR")
+library("stringr") 
+library("ggpubr")
+library("bnlearn")
+library("rattle")
+library("ggRandomForests") # ggplot2 random forest figures (This!)
+library(readr)
+library("igraph")
+library("randomForestExplainer")
+library("neuralnet")
+library("pheatmap")
+library("sdcMicro")
+library("summarytools")
+library("ggpmisc")
+library(ggfortify)
+library("pheatmap")
+library("ggRandomForests")
+library("forecast")
+library("sdcMicro")
+library("pmhtutorial")
+library("marima")
+library(randomForest)
+library(xts)
+library(ggplot2)
+
+library(feather) # data import
+library(data.table) # data handle
+library(rpart) # decision tree method
+library(party) # decision tree method
+library(forecast) # forecasting methods
+library(randomForest) # ensemble learning method
+library(ggplot2) # visualizations
+library(MTS)
+library(ggpubr)
+library(tseries)
+library("ie2misc")
+library("Metrics")
+library(tseries)
+library("eseis")
