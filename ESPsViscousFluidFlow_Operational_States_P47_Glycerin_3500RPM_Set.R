be68bd9f78c07081221763bff927de1cb524af70
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2025-09-30 12:42:22 -0300

    Update ESPsViscousFluidFlow_Operational_States_P47_Glycerin_3500RPM_Set.R

--- ESPsViscousFluidFlow_Operational_States_P47_Glycerin_3500RPM_Set.R
+++ ESPsViscousFluidFlow_Operational_States_P47_Glycerin_3500RPM_Set.R
@@ -65,19 +65,50 @@ png(filename=paste(output_dir,"Normalized_Discrete_Performance_P47_3500RPM_Glyce
   ggarrange(p1, p2, p3, labels = c("A", "B","C"),    ncol = , nrow = 3)
 dev.off()
 #################################################################
-# n=Low|BHP=Low|H=High
-# n=Low|BHP=High|H=Low
-# n=Medium|BHP=Low|H=High
-# n=Medium|BHP=Medium|H=Medium
-# n=Medium|BHP=High|H=Low
-# n=High|BHP=Medium|H=Medium
-# n=High|BHP=High|H=Low
+#1  n=Low|BHP=Low|H=High
+#2 n=Low|BHP=High|H=Low
+#3 n=Medium|BHP=Low|H=High
+#4 n=Medium|BHP=Medium|H=Medium
+#5 n=Medium|BHP=High|H=Low
+#6 n=High|BHP=Medium|H=Medium
+#7 n=High|BHP=High|H=Low
 # Set the discrete values of Q, Tm.i, Tm.o, P1, P2, Ⲅ, ρ, μi, μo
-merge_water_viscous_sub[which(merge_water_viscous_sub$operational_states=="n=Low|BHP=Low|H=High"),]
+
+# Take the tertiles
+input_variables_tertiles<-as.data.frame(lapply(merge_water_viscous_sub[,c("Flow.rate", "Average.Inlet.Temp.Tm.i", "Average.Outlet.Temp.Tm.o", "Inlet.Pressure.P1", "Outlet.Pressure.P2", "Shaft.Torque", "Inlet.Density.ρi", "Inlet.Viscosity.mi", "Outlet.Viscosity.mo")], tertile))
+
+# Reset-colnames
+colnames(input_variables_tertiles)<-c("Q_a", "Tm.i", "Tm.o", "P1", "P2", "T", "pi", "mi", "mo")
+
+# Merge data.franmes
+merge_water_viscous_sub<-cbind(merge_water_viscous_sub,input_variables_tertiles)
+
+# Take the discrete valuers
+merge_water_viscous_sub[which(merge_water_viscous_sub$operational_states=="n=Low|BHP=Low|H=High"),c("Q_a", "Tm.i", "Tm.o", "P1", "P2", "T", "pi", "mi", "mo")]
+
+
+
+
 
 #################################################################
-
-
+OS1<-unique(merge_water_viscous_sub[which(merge_water_viscous_sub$operational_states=="n=Low|BHP=Low|H=High"),c("Q_a", "Tm.i", "Tm.o", "P1", "P2", "T", "pi", "mi", "mo")])
+OS2<-unique(merge_water_viscous_sub[which(merge_water_viscous_sub$operational_states=="n=Low|BHP=High|H=Low"),c("Q_a", "Tm.i", "Tm.o", "P1", "P2", "T", "pi", "mi", "mo")])
+OS3<-unique(merge_water_viscous_sub[which(merge_water_viscous_sub$operational_states=="n=Medium|BHP=Low|H=High"),c("Q_a", "Tm.i", "Tm.o", "P1", "P2", "T", "pi", "mi", "mo")])
+OS4<-unique(merge_water_viscous_sub[which(merge_water_viscous_sub$operational_states=="n=Medium|BHP=Medium|H=Medium"),c("Q_a", "Tm.i", "Tm.o", "P1", "P2", "T", "pi", "mi", "mo")])
+OS5<-unique(merge_water_viscous_sub[which(merge_water_viscous_sub$operational_states=="n=Medium|BHP=High|H=Low"),c("Q_a", "Tm.i", "Tm.o", "P1", "P2", "T", "pi", "mi", "mo")])
+OS6<-unique(merge_water_viscous_sub[which(merge_water_viscous_sub$operational_states=="n=High|BHP=Medium|H=Medium"),c("Q_a", "Tm.i", "Tm.o", "P1", "P2", "T", "pi", "mi", "mo")])
+OS7<-unique(merge_water_viscous_sub[which(merge_water_viscous_sub$operational_states=="n=High|BHP=High|H=Low"),c("Q_a", "Tm.i", "Tm.o", "P1", "P2", "T", "pi", "mi", "mo")])
+#################################################################
+OS1$Operationa_State<-"n=Low|BHP=Low|H=High"
+OS2$Operationa_State<-"n=Low|BHP=High|H=Low"
+OS3$Operationa_State<-"n=Medium|BHP=Low|H=High"
+OS4$Operationa_State<-"n=Medium|BHP=Medium|H=Medium"
+OS5$Operationa_State<-"n=Medium|BHP=High|H=Low"
+OS6$Operationa_State<-"n=High|BHP=Medium|H=Medium"
+OS7$Operationa_State<-"n=High|BHP=High|H=Low"
+#################################################################
+# Save results trable                                                  # 
+write.table(data.frame(rbind(OS1,OS2,OS3,OS4,OS5,OS6,OS7)),   paste(output_dir,"ESPsViscousFluidFlow_Operationalç_state.tsv",sep="/"), na = "NA", append = FALSE, col.names = TRUE, row.names = TRUE, sep = "\t", quote = TRUE)
 
 
 
