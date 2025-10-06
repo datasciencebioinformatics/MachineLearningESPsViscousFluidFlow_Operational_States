7bff87d95c34964bd27cf99425ead32f533f76a5
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2025-10-06 02:05:55 -0300

    Create ESPsViscousFluidFlow_Operational_States_P47_Glycerin_3500RPM_Performance_curves.R

---
+++ ESPsViscousFluidFlow_Operational_States_P47_Glycerin_3500RPM_Performance_curves.R
@@ -0,0 +1,61 @@
+################################################################################################################
+# To Do:
+# reproduce Fig. 7—ESP P47 performance pumping viscous fluid at 3,500 rev/min.
+# This script will add a collumn to the metafile with the operational state
+# The operational state will be defined as the n=state-h=state-bhp=state
+merge_water_viscous_sub<-merge_water_viscous[which(merge_water_viscous$equip=="P47" & merge_water_viscous$fluid == "Glycerin" & merge_water_viscous$RPM=="3500" & merge_water_viscous$Inlet.Viscosity == "128"),]
+
+# Remove units from bhp, head and efficiency and put them in a single plot
+merge_water_viscous_normalized<-as.data.frame(lapply(merge_water_viscous_sub[,c("n","BHP","H","Q")], normalize))
+
+# Renames collumns
+colnames(merge_water_viscous_normalized)<-c("n_normalized","BHP_normalized","H_normalized","Q_normalized")
+
+# Merge tables
+merge_water_viscous_sub<-cbind(merge_water_viscous_sub,merge_water_viscous_normalized)
+
+# Add stepp
+merge_water_viscous_sub$step<-1:dim(merge_water_viscous_sub)[1]
+
+# Take collumns for the plot
+merge_water_viscous_plot<-merge_water_viscous_sub[,c("n_normalized","BHP_normalized","H_normalized","Q_normalized","step")]
+
+# Rename collumns
+colnames(merge_water_viscous_plot)<-c("n","BHP","H","Q","step")
+
+# Melt table
+merge_water_viscous_sub_melt<-reshape2::melt(merge_water_viscous_plot,id.vars=c("step","Q"))
+
+# Generate plot
+p1<-ggplot(merge_water_viscous_sub_melt, aes(x=Q, y=value, shape=variable)) +   geom_point(aes(color=variable)) +  theme_bw() + theme(legend.position = "bottom") + ggtitle("P47, 3550RPM, Glycerin, inlet viscosituy 128") +  geom_line(aes(color=variable)) + ylab("Normalized values (H, BHP, n)") +  xlab("Normalized Q") 
+
+# To do :
+# Save this plot
+################################################################################################################
+# To do :
+# 1) Define the three classes for efficiency (low, medium, high)
+# 2) Organize the ranges for low, medium, high
+
+# Take the tertiles
+merge_water_viscous_sub_tertiles<-as.data.frame(lapply(merge_water_viscous_sub[,c("n","BHP","H","Q")], tertile))
+
+# Renames collumns
+colnames(merge_water_viscous_sub_tertiles)<-c("n_discrete","BHP_discrete","H_discrete","Q_discrete")
+################################################################################################################
+# Fig. 7—ESP P47 performance pumping viscous fluid at 3,500 rev/min.
+ESP_P47_water_plot_Q_H <- ggplot(merge_water_viscous_sub, aes(x = Q, y = H))     + geom_point(aes(color=n_discrete)) + theme_bw()   + ggtitle ("Flow rate Q vs. Head H")    + ylab("Head H [m]")                   + labs(x = expression("Flow rate Q [" * m^3/h * "]")) + theme(legend.position = "bottom")  
+ESP_P47_water_plot_BHP <- ggplot(merge_water_viscous_sub, aes(x = Q, y = BHP))   + geom_point(aes(color=n_discrete)) + theme_bw()   + ggtitle ("Flow rate Q vs. Power BHP") + ylab("Power BHP [W]")                + labs(x = expression("Flow rate Q [" * m^3/h * "]")) + theme(legend.position = "none")    
+ESP_P47_water_plot_n   <- ggplot(merge_water_viscous_sub, aes(x = Q, y = n*100)) + geom_point(aes(color=n_discrete)) + theme_bw()   + ggtitle ("Flow rate Q vs. Efficiency n") + ylab("Efficiency n [%]")          + labs(x = expression("Flow rate Q [" * m^3/h * "]"))   + theme(legend.position = "bottom")      
+
+################################################################################################################
+# Melt tabele
+# Plot_raw_vibration_data.png                                                                                                            
+png(filename=paste(project_folder,"ESP_P47_dilluted_glucerin_RPM3500_Viscosity_128.png",sep=""), width = 20, height = 25, res=600, units = "cm")  
+  ggarrange(ESP_P47_water_plot_Q_H,ESP_P47_water_plot_BHP,ESP_P47_water_plot_n, nrow =3,common.legend = TRUE,legend="bottom")
+dev.off()
+
+# Merge tables
+# Define the ranges of efficiencies n for these vectors
+low_efficiency<-merge_water_viscous_sub[which(merge_water_viscous_sub$n_discrete=="Low"),]
+medium_efficiency<-merge_water_viscous_sub[which(merge_water_viscous_sub$n_discrete=="Medium"),]
+high_efficiency<-merge_water_viscous_sub[which(merge_water_viscous_sub$n_discrete=="High"),]
