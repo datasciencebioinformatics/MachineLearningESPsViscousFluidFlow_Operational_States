cfb3a51ab38ce1fdbf4377593bdcf9789be33a4c
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2025-10-24 14:16:06 -0300

    Update ESPsViscousFluidFlow_Operational_States_Simluate_TimeSeries_From_P47_Glycerin_3500RPM.R

--- ESPsViscousFluidFlow_Operational_States_Simluate_TimeSeries_From_P47_Glycerin_3500RPM.R
+++ ESPsViscousFluidFlow_Operational_States_Simluate_TimeSeries_From_P47_Glycerin_3500RPM.R
@@ -16,6 +16,7 @@ colnames(merge_water_viscous_sub)<-c("Time","Q", "Tm.i", "Tm.o", "P1", "P2", "T"
 # Convert RPM to numeric
 merge_water_viscous_sub$RPM<-as.numeric(merge_water_viscous_sub$RPM)
 
+###############################################################################################################################################################################
 # Mett data.frame
 melt_water_viscous_sub<-reshape2::melt(merge_water_viscous_sub[,c("Time","Q", "Tm.i", "Tm.o", "P1", "P2", "T", "pi", "mi", "mo", "RPM", "n", "H", "BHP")],id.vars=c("Time"))
 
@@ -28,6 +29,10 @@ png(filename=paste(project_folder,"Reference_time_series.png",sep=""), width = 1
   p
 dev.off()
 ####################################################################################################################################################################################
+# Split the dataset in training set and testing set
+merge_water_viscous_trainning<-merge_water_viscous_sub[merge_water_viscous_sub$Inlet.Viscosity!=128,]
+merge_water_viscous_testing<-merge_water_viscous_sub[merge_water_viscous_sub$Inlet.Viscosity!=128,]
+
 # Simulations of Well Sanding (Pump Plugging).
 # First, simulate each variable in function of Q
 # Start df with the results
@@ -45,12 +50,21 @@ for (variable in c("Tm.i","Tm.o","P1","P2","T","pi","mi","mo"))
     Formula_variable_versus_Q<-as.formula(paste(variable," ~ Q",sep=""))
 
     # Set random forest morel
-    rf_variable_versus_Q   <- train(Formula_variable_versus_Q, data = merge_water_viscous_sub, method = "rf" )         # K-Nearest Neighbors (KNN)                     Ok                                                                                     
+    rf_variable_versus_Q   <- train(Formula_variable_versus_Q, data = merge_water_viscous_trainning, method = "rf" )         # K-Nearest Neighbors (KNN)                     Ok                                                                                     
 
     # Calculate predictions
-    rf_variable_versus_prediction<-predict(rf_variable_versus_Q , merge_water_viscous_sub)
+    rf_variable_versus_prediction<-predict(rf_variable_versus_Q , merge_water_viscous_testing)
 
     # Add results of the variable
     df_predicted_results<-rbind(df_predicted_results,data.frame(Time=1:length(rf_variable_versus_prediction),Value=rf_variable_versus_prediction,variable=variable))
-    
 }
+####################################################################################################################################################################################
+# Most basic bubble plot
+p2 <- ggplot(df_predicted_results, aes(x=Time, y=Value)) +  geom_line() +   facet_grid(rows = vars(variable),scales="free") + theme_bw()  + ggtitle ("Random forest predicted time-series")
+
+# Melt tabele
+# Plot_raw_vibration_data.png                                                                                                            
+png(filename=paste(project_folder,"Reference_time_series_ranfom_forest.png",sep=""), width = 15, height = 20, res=600, units = "cm")  
+  p2
+dev.off()
+####################################################################################################################################################################################
