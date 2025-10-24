4e42ec4a5f9cfd88a90f0a54724c6c9c584cfbe0
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2025-10-24 14:53:31 -0300

    Update ESPsViscousFluidFlow_Operational_States_Simluate_TimeSeries_From_P47_Glycerin_3500RPM.R

--- ESPsViscousFluidFlow_Operational_States_Simluate_TimeSeries_From_P47_Glycerin_3500RPM.R
+++ ESPsViscousFluidFlow_Operational_States_Simluate_TimeSeries_From_P47_Glycerin_3500RPM.R
@@ -30,18 +30,20 @@ png(filename=paste(project_folder,"Reference_time_series.png",sep=""), width = 1
 dev.off()
 ####################################################################################################################################################################################
 # Split the dataset in training set and testing set
-merge_water_viscous_trainning<-merge_water_viscous_sub[merge_water_viscous_sub$Inlet.Viscosity!=128,]
-merge_water_viscous_testing<-merge_water_viscous_sub[merge_water_viscous_sub$Inlet.Viscosity!=128,]
+merge_water_viscous_trainning<-merge_water_viscous_sub[merge_water_viscous_sub$RPM!=3500,]
+merge_water_viscous_testing<-merge_water_viscous_sub[merge_water_viscous_sub$RPM==3500,]
 
 # Simulations of Well Sanding (Pump Plugging).
 # First, simulate each variable in function of Q
 # Start df with the results
 df_predicted_results<-data.frame(Time=c(),Value=c(),variable=c())
 
+#tuneGrid_rf <- expand.grid(mtry = seq(1, 50, by = 5))
+
 # The rows are increasing viscosity values and the collumns the increasing time value
 # Convert the P47_viscous_3500_data_sub to time-series for each variable
 # For each variable 
-for (variable in c("Tm.i","Tm.o","P1","P2","T","pi","mi","mo"))
+for (variable in c("Tm.i","Tm.o","P1","P2","T","pi","mi","mo","RPM"))
 {
     # Set formula for predicting the variable in function of Q
     Formula_variable_versus_Q<-as.formula(paste(variable," ~ Q",sep=""))
@@ -53,17 +55,24 @@ for (variable in c("Tm.i","Tm.o","P1","P2","T","pi","mi","mo"))
     rf_variable_versus_prediction<-predict(rf_variable_versus_Q , merge_water_viscous_testing)
 
     # Add results of the variable
-    df_predicted_results<-rbind(df_predicted_results,data.frame(Time=1:length(rf_variable_versus_prediction),value=rf_variable_versus_prediction,variable=variable))
+    df_predicted_results<-rbind(df_predicted_results,data.frame(Time=merge_water_viscous_testing$Time,value=rf_variable_versus_prediction,variable=variable))
 }
 ####################################################################################################################################################################################
+# Mett data.frame
+melt_water_viscous_testing<-reshape2::melt(merge_water_viscous_testing[,c("Time","Q", "Tm.i", "Tm.o", "P1", "P2", "T", "pi", "mi", "mo", "RPM")],id.vars=c("Time"))
+####################################################################################################################################################################################
 # Add data type
-melt_water_viscous_sub$type   <-"simulated"
+melt_water_viscous_testing$type   <-"simulated"
 df_predicted_results$type     <-"experimental"
 
-rbind(df_predicted_results,df_predicted_results)
+# Merge datasets
+merged_predicted_results<-rbind(df_predicted_results,melt_water_viscous_testing)
+
+# Relevel factors
+merged_predicted_results$variable<-factor(merged_predicted_results$variable,levels=c(c("Q","RPM", "Tm.i", "Tm.o", "P1", "P2", "T", "pi", "mi", "mo")))
 
 # Most basic bubble plot
-p2 <- ggplot(df_predicted_results, aes(x=Time, y=value)) +  geom_line() +   facet_grid(rows = vars(variable),scales="free") + theme_bw()  + ggtitle ("Random forest predicted time-series")
+p2 <- ggplot(merged_predicted_results, aes(x=Time, y=value,group = type, color = type)) +  geom_line() +   facet_grid(rows = vars(variable),scales="free") + theme_bw()  + ggtitle ("Random forest predicted time-series") + scale_colour_brewer(palette = "Set1")
 
 # Melt tabele
 # Plot_raw_vibration_data.png                                                                                                            
