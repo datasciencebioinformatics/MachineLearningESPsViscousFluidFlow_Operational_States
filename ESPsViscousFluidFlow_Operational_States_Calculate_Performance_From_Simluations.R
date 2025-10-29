8e84768c9830f92a07d0e366456a661a8c81aa63
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2025-10-29 10:13:03 -0300

    Update ESPsViscousFluidFlow_Operational_States_Calculate_Performance_From_Simluations.R

--- ESPsViscousFluidFlow_Operational_States_Calculate_Performance_From_Simluations.R
+++ ESPsViscousFluidFlow_Operational_States_Calculate_Performance_From_Simluations.R
@@ -254,7 +254,7 @@ for (decay in df_simulated_input_variables$decay)
     # Plot_raw_vibration_data.png                                                                                                            
     png(filename=paste(project_folder,"ESPsViscousFluidFlow_Pheatmap_simulated_",decay,".png",sep=""), width = 30, height = 30, res=600, units = "cm")  
       # Add annotation : bhp, head, efficiency
-      pheatmap(decay_data[,c("Q", "Tm.i", "Tm.o", "P1", "P2", "T", "pi", "mi", "mo", "RPM")] , show_rownames = F,annotation_row = annotation_row_exp,annotation_colors=ann_colors,cluster_rows = FALSE, main=paste("decay",decay,sep=" = "),scale ="column")
+      pheatmap(decay_data[,c("Q", "Tm.i", "Tm.o", "P1", "P2", "T", "pi", "mi", "mo", "RPM")] , show_rownames = F,annotation_row = annotation_row_exp,annotation_colors=ann_colors,cluster_rows = FALSE, main=paste("decay",decay,sep=" = "))
     dev.off()
 }
 
