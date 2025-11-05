85ed235bfbaca16e71d2ee743cb2192fd1068fef
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2025-11-05 07:49:36 -0500

    Update ESPsViscousFluidFlow_Operational_States_Broken_Shaft_Bayesian_Networks_Random_Forest.R

--- ESPsViscousFluidFlow_Operational_States_Broken_Shaft_Bayesian_Networks_Random_Forest.R
+++ ESPsViscousFluidFlow_Operational_States_Broken_Shaft_Bayesian_Networks_Random_Forest.R
@@ -1,4 +1,3 @@
-####################################################################################################################################################################################
 # TO DO : try all with the 5800 points.
 # This script will add a collumn to the metafile with the operational state
 # The operational state will be defined as the n=state-h=state-bhp=state
@@ -22,6 +21,55 @@ trainned_rf_models<-list()
 # Split the dataset in training set and testing set
 merge_water_viscous_trainning<-merge_water_viscous_sub[merge_water_viscous_sub$RPM==3000 ,]
 merge_water_viscous_testing  <-merge_water_viscous_sub[merge_water_viscous_sub$RPM==3500 ,]
+
+# Simulations of Well Sanding (Pump Plugging).
+# First, simulate each variable in function of Q
+# Start df with the results
+df_predicted_results<-data.frame(Time=c(),Value=c(),variable=c())
+
+# The rows are increasing viscosity values and the collumns the increasing time value
+# Convert the P47_viscous_3500_data_sub to time-series for each variable
+# For each variable 
+for (variable in c("Q","Tm.i","Tm.o","P1","P2","T","pi","mi","mo","RPM"))
+{
+    # Set formula for predicting the variable in function of Q
+    Formula_variable_versus_n<-as.formula(paste(variable," ~ n",sep=""))
+
+    # Set random forest morel
+    rf_variable_versus_n   <- train(Formula_variable_versus_n, data = merge_water_viscous_trainning, method = "rf" )         # K-Nearest Neighbors (KNN)                     Ok   
+
+    # Store the trained model
+    trainned_rf_models[[variable]]<-rf_variable_versus_n
+  
+    # Calculate predictions
+    rf_variable_versus_prediction<-predict(rf_variable_versus_n , merge_water_viscous_testing)
+
+    # Add results of the variable
+    df_predicted_results<-rbind(df_predicted_results,data.frame(Time=merge_water_viscous_testing$Time,value=rf_variable_versus_prediction,variable=variable))
+}
+####################################################################################################################################################################################
+# Mett data.frame
+melt_water_viscous_testing<-reshape2::melt(merge_water_viscous_testing[,c("Time","n","Q", "Tm.i", "Tm.o", "P1", "P2", "T", "pi", "mi", "mo", "RPM")],id.vars=c("Time"))
+####################################################################################################################################################################################
+# Add data type
+melt_water_viscous_testing$type   <-"experimental"
+df_predicted_results$type         <-"simulated"
+
+# Merge datasets
+merged_predicted_results<-rbind(df_predicted_results,melt_water_viscous_testing)
+
+# Relevel factors
+merged_predicted_results$variable<-factor(merged_predicted_results$variable,levels=c(c("n","Q","RPM", "Tm.i", "Tm.o", "P1", "P2", "T", "pi", "mi", "mo")))
+####################################################################################################################################################################################
+# Add data type
+df_predicted_results$type     <-"experimental"
+df_predicted_results$type     <-"simulated"
+
+# Merge datasets
+merged_predicted_results<-rbind(df_predicted_results,melt_water_viscous_testing)
+
+# Relevel factors
+merged_predicted_results$variable<-factor(merged_predicted_results$variable,levels=c(c("n","Q","RPM", "Tm.i", "Tm.o", "P1", "P2", "T", "pi", "mi", "mo")))
 ####################################################################################################################################################################################
 # Simulate time-series for Broken Shaft
 ####################################################################################################################################################################################
