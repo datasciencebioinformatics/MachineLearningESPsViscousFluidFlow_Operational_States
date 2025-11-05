29ed6c2aaccca0addd2cbeb83299f8f590352650
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2025-11-05 08:03:26 -0500

    Update ESPsViscousFluidFlow_Operational_States_Broken_Shaft_Bayesian_Networks_Random_Forest.R

--- ESPsViscousFluidFlow_Operational_States_Broken_Shaft_Bayesian_Networks_Random_Forest.R
+++ ESPsViscousFluidFlow_Operational_States_Broken_Shaft_Bayesian_Networks_Random_Forest.R
@@ -113,3 +113,195 @@ png(filename=paste(project_folder,"Broken_Shaft_Simulated_Declining_Flow_Rate_n_
     ) +
     theme_minimal()
 dev.off()
+
+
+# First, simulated values of Q are generate by simulation.
+# Second the values of input variables are predicted from the simulated Q.
+# Thirds, the values were used to generate decision  trees.
+# Only 29 points are used to generate the decision tree.
+# More points can be used.
+# One way is to increase the range and skip the reference data.
+####################################################################################################################################################################################
+# Simulations of Well Sanding (Pump Plugging).
+# First, simulate each variable in function of Q
+# Start df with the results
+df_predicted_results<-data.frame(Time=c(),Value=c(),variable=c(),b=c())
+
+# The rows are increasing viscosity values and the collumns the increasing time value
+# Convert the P47_viscous_3500_data_sub to time-series for each variable
+# For each variable 
+for (b in levels(factor(sim_data$b)))
+{   
+  # The rows are increasing viscosity values and the collumns the increasing time value
+  # Convert the P47_viscous_3500_data_sub to time-series for each variable
+  # For each variable 
+  for (variable in c("Q","Tm.i","Tm.o","P1","P2","T","pi","mi","mo","RPM"))
+  {  
+      # Store the trained model
+      rf_variable_versus_n<-trainned_rf_models[[variable]]
+    
+      # Calculate predictions
+      rf_variable_versus_prediction<-predict(rf_variable_versus_n , data.frame(n=sim_data[sim_data$b==b,"Noisy_Value"]))
+  
+      # Add results of the variable
+      df_predicted_results<-rbind(df_predicted_results,data.frame(Time=time_points,value=rf_variable_versus_prediction,variable=variable,b=b))
+
+  }
+  # Add results of the variable
+  df_predicted_results<-rbind(df_predicted_results,data.frame(Time=time_points,value=sim_data[sim_data$b==b,"Noisy_Value"],variable="n",b=b))
+}
+####################################################################################################################################################################################
+# Add also simulated data
+####################################################################################################################################################################################
+# Relevel factors
+df_predicted_results$variable<-factor(df_predicted_results$variable,levels=c(c("n","Q","RPM", "Tm.i", "Tm.o", "P1", "P2", "T", "pi", "mi", "mo")))
+
+# Relevel factors
+df_predicted_results$b<-factor(df_predicted_results$b)
+
+####################################################################################################################################################################################
+# Add also simulated data
+####################################################################################################################################################################################
+# Relevel factors
+df_predicted_results$variable<-factor(df_predicted_results$variable,levels=c(c("n","Q","RPM", "Tm.i", "Tm.o", "P1", "P2", "T", "pi", "mi", "mo")))
+
+# Relevel factors
+df_predicted_results$b<-factor(df_predicted_results$b)
+
+# Most basic bubble plot
+p2 <- ggplot(df_predicted_results, aes(x=Time, y=value,group = b, color = b)) +  geom_line() +   facet_grid(rows = vars(variable),scales="free") + theme_bw()  + ggtitle ("Random forest predicted time-series") + scale_colour_brewer(palette = "Set1")
+
+# Melt tabele
+# Plot_raw_vibration_data.png                                                                                                            
+png(filename=paste(project_folder,"Broken_Shaft_Simulated_time_series_ranfom_forest.png",sep=""), width = 15, height = 20, res=600, units = "cm")  
+  p2
+dev.off()
+####################################################################################################################################################################################
+# Calculate performance, head and BHP for each configurations.
+####################################################################################################################################################################################
+n    <- df_predicted_results[which(df_predicted_results$variable=="n"),"value"]
+Q    <- df_predicted_results[which(df_predicted_results$variable=="Q"),"value"]
+Tm.i <- df_predicted_results[which(df_predicted_results$variable=="Tm.i"),"value"]
+Tm.o <- df_predicted_results[which(df_predicted_results$variable=="Tm.o"),"value"]
+P1   <-df_predicted_results[which(df_predicted_results$variable=="P1"),"value"]
+P2   <-df_predicted_results[which(df_predicted_results$variable=="P2"),"value"]
+T    <-df_predicted_results[which(df_predicted_results$variable=="T"),"value"]
+pi   <- df_predicted_results[which(df_predicted_results$variable=="pi"),"value"]
+mi   <-  df_predicted_results[which(df_predicted_results$variable=="mi"),"value"]
+mo   <- df_predicted_results[which(df_predicted_results$variable=="mo"),"value"]
+RPM  <- df_predicted_results[which(df_predicted_results$variable=="RPM"),"value"]
+Time <- df_predicted_results[which(df_predicted_results$variable=="RPM"),"Time"]
+b<- df_predicted_results[which(df_predicted_results$variable=="RPM"),"b"]
+
+# Compile input variables
+df_simulated_input_variables<-data.frame(Time=Time,n=n, Q=Q, Tm.i=Tm.i, Tm.o=Tm.o, P1=P1, P2=P2, T=T, pi=pi, mi=mi, mo=mo, RPM=RPM, b=b )
+
+# set  the gravitational constant
+# 9.81 meters per second squared (m/s2) is the approximate value of the acceleration due to gravity on Earth's surface. This value is represented by the letter g. 
+g=9.81
+
+# Number of stages (constant equanto to 3)
+N   <-3 
+
+# Store the useful power Ph
+df_simulated_input_variables$P_h<-0
+
+# The pump efficiency (n) is defined as:
+# n = efficiency, dimensionless [%]
+#df_simulated_input_variables$n<-0
+
+# Store the pump head
+# H = head, L, m
+df_simulated_input_variables$H<-0
+
+# Store the shaft torque is the mechanical parameter used to calculate the driving power or brake horsepower (BHP)
+# BHP = mL^2t^–3, watts
+df_simulated_input_variables$BHP<-0
+
+# Compute the delta pressure
+df_simulated_input_variables$Delta.Pressure<-df_simulated_input_variables$P2-df_simulated_input_variables$P1
+
+# First, Calculate the velocity
+for (measure in rownames(df_simulated_input_variables))
+{ 
+  # Model
+  model=df_simulated_input_variables[measure,"equip"]
+
+  # Impeller Diameter mm
+  # Unit checked mm
+  D=unique(metada_data[which(metada_data$model == "P47"),2]) * 1000 # m converted to mm
+    
+  # Store inlet and outlet pressure
+  # P = pressure, mL–1t–2, Pa
+  # Unit checked pa
+  P1  <-as.numeric(df_simulated_input_variables[measure,"P1"])  * 100000 # Bar converted to pa
+  P2  <-as.numeric(df_simulated_input_variables[measure,"P2"]) * 100000 # Bar converted to pa
+  
+  # Density
+  # p = density, mL–3, kg/m3
+  # Unit checked kg/m3
+  p  <-as.numeric(df_simulated_input_variables[measure,"pi"])
+
+  # The Flow Rate, Q 
+  # Q = volumetric flow rate, L3, t–1, m3/s
+  # Unit checked m3/h
+  #df_simulated_input_variables[measure,"Q"]<-as.numeric(df_simulated_input_variables[measure,"Q"])/(p) # kg/h converted to m3/s
+
+  # Head pump
+  # H = head, L, m
+  df_simulated_input_variables[measure,"H"]<-((P2-P1)/(p*g))*(1/N) 
+
+  # The rotational speed w in rads/s
+  # unit checked RPM
+  w=as.numeric(df_simulated_input_variables[measure,"RPM"])*0.10472
+  
+  # Net.Shaft.Torque
+  # T = shaft torque, mL2,t–2, N·m
+  # unit checked N·m
+  T=as.numeric(df_simulated_input_variables[measure,"T"])
+
+  # ESP is the BHP
+  # BHP = mL^2t^–3, watts
+  df_simulated_input_variables[measure,"BHP"]=(1/N)*(w*T)
+
+  #P_{h}: The power in kilowatts (kW) 
+  # Q    : The flow capacity in cubic meters per hour (m3/h) 
+  # p    : The density of the fluid in kilograms per cubic meter (kg/m3) 
+  # g    : The acceleration due to gravity (9.81m/s2) 
+  # H    : The differential head in meters (m)
+  
+  # useful power Ph 
+  # Flow rate in m3/s
+  df_simulated_input_variables[measure,"P_h"]<- (p*g*df_simulated_input_variables[measure,"H"]*(df_simulated_input_variables[measure,"Q"]*(1/3600)))
+
+  # The pump efficiency (n) is defined as:
+  # n = efficiency, dimensionless [%]
+  #df_simulated_input_variables[measure,"n"] <- df_simulated_input_variables[measure,"P_h"]/df_simulated_input_variables[measure,"BHP"]
+}
+#######################################################################################################
+# add plot
+melt_simulated_input_variables<-reshape2::melt(df_simulated_input_variables,id.vars=c("Time","b")) 
+
+# Most basic bubble plot
+p3 <- ggplot(melt_simulated_input_variables[melt_simulated_input_variables$variable %in% c("n","H","BHP"),], aes(x=Time, y=value,group = b, color = b)) +  geom_line() +   facet_grid(rows = vars(variable),scales="free") + theme_bw()  + ggtitle ("Random forest predicted time-series - performance variables") + scale_colour_brewer(palette = "Set1")
+
+# Melt tabele
+# Plot_raw_vibration_data.png                                                                                                            
+png(filename=paste(project_folder,"Broken_Shaft_Simulated_performance_variables_ranfom_forest.png",sep=""), width = 15, height = 10, res=600, units = "cm")  
+  p3
+dev.off()
+#######################################################################################################
+
+
+#######################################################################################################
+# Fig. 7—ESP P47 performance pumping viscous fluid at 3,500 rev/min.
+ESP_P47_water_plot_Q_H <- ggplot(df_simulated_input_variables[,c("Q","n","H","BHP","b")], aes(x = Q, y = H,colour=b))     + geom_point() + geom_line()  + theme_bw()   + ggtitle ("Flow rate Q vs. Head H")    + ylab("Head H [m]")                   + labs(x = expression("Flow rate Q [" * m^3/h * "]")) + theme(legend.position = "bottom")  
+ESP_P47_water_plot_BHP <- ggplot(df_simulated_input_variables[,c("Q","n","H","BHP","b")], aes(x = Q, y = BHP,colour=b))   + geom_point() + geom_line() + theme_bw()   + ggtitle ("Flow rate Q vs. Power BHP") + ylab("Power BHP [W]")                + labs(x = expression("Flow rate Q [" * m^3/h * "]")) + theme(legend.position = "none")    
+ESP_P47_water_plot_n   <- ggplot(df_simulated_input_variables[,c("Q","n","H","BHP","b")], aes(x = Q, y = n*100,colour=b)) + geom_point() + geom_line() + theme_bw()   + ggtitle ("Flow rate Q vs. Efficiency n") + ylab("Efficiency n [%]")          + labs(x = expression("Flow rate Q [" * m^3/h * "]"))   + theme(legend.position = "bottom")      
+
+# Melt tabele
+# Plot_raw_vibration_data.png                                                                                                            
+png(filename=paste(project_folder,"Broken_Shaft_ESP_P47_dilluted_glucerin_Operational_states.png",sep=""), width = 20, height = 25, res=600, units = "cm")  
+  ggarrange(ESP_P47_water_plot_Q_H,ESP_P47_water_plot_BHP,ESP_P47_water_plot_n, nrow =3,common.legend = TRUE,legend="bottom")
+dev.off()
+#######################################################################################################
